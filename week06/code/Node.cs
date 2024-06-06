77929705a9fdd57d7e2884bf890199faf3b8d57c
Ryan Singleton <ryansingleton@Ryans-MacBook-Pro.local>
2024-06-06 14:29:16 -0400

    finished week 6 assignment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,36 +1,66 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+    public void Insert(int value)
+    {
+        if (value == Data)
+            return;
+        else
+        {
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
-        else {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left is not null && Left.Contains(value);
+        else
+            return Right is not null && Right.Contains(value);
+    
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left is null && Right is null)
+            return 1;
+        else if (Left is null)
+            return 1 + Right.GetHeight();
+        else if (Right is null)
+            return 1 + Left.GetHeight();
+        else
+        {
+            int leftHeight = Left.GetHeight();
+            int rightHeight = Right.GetHeight();
+            return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+        }
     }
 }
